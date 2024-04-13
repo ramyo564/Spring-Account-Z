99e4399bc4a4b60ad15f96455e631d4fbee91e12
ramyo564 <yohan032yohan@gmail.com>
2024-04-13 18:30:47 +0900

    test : User 성공 케이스 테스트 코드

---
+++ src/test/java/com/example/accountz/webController/SuccessUserControllerTest.java
@@ -0,0 +1,127 @@
+package com.example.accountz.webController;
+
+import com.example.accountz.model.UserDto;
+import com.example.accountz.persist.entity.UserEntity;
+import com.example.accountz.persist.repository.UserRepository;
+import com.example.accountz.security.TokenProvider;
+import com.example.accountz.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(UserController.class)
+class SuccessUserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @WithMockUser
+    @Test
+    void signUp() throws Exception {
+        //given
+        given(userRepository.existsByEmail(anyString()))
+                .willReturn(false);
+        given(passwordEncoder.encode(anyString()))
+                .willReturn("encodedPassword");
+
+        //when
+        UserDto.SignUp signUpDto = new UserDto.SignUp();
+        signUpDto.setName("John");
+        signUpDto.setEmail("john@example.com");
+        signUpDto.setPassword("password");
+        signUpDto.setBirthDay(
+                LocalDate.of(1990, 5, 15));
+
+        // 결과 확인
+        ResultActions resultActions =
+                mockMvc.perform(
+                        post("/auth/sign-up")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(
+                                        signUpDto)
+                                )
+                        )
+                //.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title")
+                    .value("회원가입"))
+                .andExpect(jsonPath("$.detail")
+                    .value("Success"));
+    }
+
+    @WithMockUser
+    @Test
+    void signIn()throws Exception {
+        // Given
+
+        UserDto.SignIn signInRequest = new UserDto.SignIn();
+        signInRequest.setEmail("123@123.com");
+        signInRequest.setPassword("12345678");
+        String token = "generated_token";
+        given(userService.authenticateUser(signInRequest))
+                .willReturn(
+                        UserEntity.builder()
+                                .id(12L)
+                                .name("qwe")
+                                .email("123@123.com")
+                                .birthDay(LocalDate.of(
+                                        1999,12,12))
+                                .password("12345678")
+                                .build());
+        given(tokenProvider.generateToken("123@123.com"))
+                .willReturn(token);
+        //when
+        //then
+        mockMvc.perform(post("/auth/sign-in")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                signInRequest)))
+                //.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title")
+                        .value("Token"))
+                .andExpect(jsonPath("$.detail")
+                .value("generated_token"));
+
+    }
+
+}
